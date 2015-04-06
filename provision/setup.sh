4560b58ca2660fa5382bb83863207b9f3be49708
Erik <erik@conceptshell.com>
2015-04-06 14:02:05 -0300

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -8,13 +8,14 @@ sudo locale-gen pt_BR.UTF-8
 #criar base de dados no postgres
 # sudo su - postgres
 # createuser --interactive -P
-# createdb --owner=nome nome
+# createdb --owner=nomedousuario nomedobanco
 
-#system packages
+# Adicione aqui se instalar algum pacote do sistema
 sudo apt-get install -y postgresql python-setuptools python-dev build-essential nginx libjpeg-dev libpng-dev python-psycopg2 python-pip
 
-#python packages
+# Adicione aqui se instalar algum pacote pelo pip
 sudo pip install django
 
+# Coloca a configuração do nginx no lugar
 sudo ln -s /vagrant/deploy/testespoiler.conf /etc/nginx/sites-enabled/testespoiler.conf
 sudo rm /etc/nginx/sites-enabled/default
