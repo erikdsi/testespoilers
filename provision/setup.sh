a7c7f99506b4d9a4495ccc95c0946b0071553c16
Erik <erik@conceptshell.com>
2015-04-12 10:23:55 -0300

    removed nginx

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-#updates the base system
+# updates the base system
 sudo apt-get -y update && sudo apt-get -y upgrade
-#generates the locale, needed so the postgresql install create a initial cluster for you
+# generates the locale, needed so the postgresql install create a initial cluster for you
 sudo locale-gen pt_BR.UTF-8
 
-#criar base de dados no postgres
+# Comandos para criar base de dados no Postgres
 # sudo su - postgres
 # createuser --interactive -P
 # createdb --owner=nomedousuario nomedobanco
 
 # Adicione aqui se instalar algum pacote do sistema
-sudo apt-get install -y postgresql python-setuptools python-dev build-essential nginx libjpeg-dev libpng-dev python-psycopg2 python-pip
+sudo apt-get install -y postgresql python-setuptools python-dev build-essential libjpeg-dev libpng-dev python-psycopg2 python-pip
 
 # Adicione aqui se instalar algum pacote pelo pip
 sudo pip install django
 
-# Coloca a configuração do nginx no lugar
-sudo ln -s /vagrant/deploy/testespoiler.conf /etc/nginx/sites-enabled/testespoiler.conf
-sudo rm /etc/nginx/sites-enabled/default
